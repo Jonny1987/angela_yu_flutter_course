4dc3464f5000d6b30062e2ba40637e75aa2621d7
Jonny1987 <jrshannie@yahoo.com>
2020-08-18 22:40:43 +0100

    removed api key

--- Clima-Flutter/lib/services/weather.dart
+++ Clima-Flutter/lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
-String apiKey = '50b34c458e8b554931a56c0e8bc1704b';
+String apiKey = 'PUT_API_KEY_HERE';
 String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
